83bc76bb496c2afe9aaa5dfe229a102cc772af88
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-02-03 17:15:08 +0300

    w

---
+++ src/routes/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <h1 class="c" style="margin-bottom: 4.5rem; font-size: 3.1rem;">База знаний</h1>
+
+  <ul class="menu">
+    <li><RouterLink to="/scratch">Scratch</RouterLink></li>
+    <li><RouterLink to="/construct-2">Construct 2</RouterLink></li>
+  </ul>
+</template>
---
+++ src/routes/Scratch.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="c" style="margin-bottom: 4.5rem;">Scratch</h1>
+
+  <p>sdfdaskafk</p>
+</template>
